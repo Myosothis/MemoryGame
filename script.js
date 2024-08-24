3509e2c7e48e4bc6263e129e693947e453643ad5
Myosothis <myosothis1@gmail.com>
2024-08-24 16:38:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let cardsValue = [1, 2, 3]; // Valeurs prédéfinies des cartes pour assigner les images.
-let flipped;
+let flipped; // État de la carte.
+let flippedCard = 0; // Compteur de cartes retournées.
+let check1 = ''; // Vérification des pairs dans la fonction isPair.
+let check2 = '';
 
 // Récupération des images via leur class name depuis le HTML.
 let deck = document.getElementsByClassName("card");
@@ -10,8 +13,8 @@ for(let card of deck) {
     card.addEventListener("click", clickImg);
 }
 
-// Assemblage des images avec leurs valeurs prédéfinies.
-for (let i in deck) {
+// Assemblage des images avec leurs valeurs prédéfinies + variable flipped.
+/*for (let i in deck) {
     deck[i].src = "img/Card" + cardsValue[i] + ".jpg";
     deck[i].flipped = true;
 }
@@ -25,13 +28,49 @@ function clickImg(e) {
 
     if (e.target.flipped = false) {
         e.target.src = "img/" + e.target.id + ".jpg";
-        console.log("WEEEEEEEEEEE");
-        e.target.unflipped = true;
+        console.log("Flipped a été réglé sur True.");
+        e.target.flipped = true;
+        console.log("Flipped a été réglé sur True.");
+        return;
     } else {
         e.target.src = "img/Card.jpg";
         e.target.style.border = "3px solid blue"
         e.target.style.borderRadius = "15px";
+        console.log("Flipped a été réglé sur False.");
         e.target.flipped = false;
+        console.log("Flipped status : " + e.target.flipped);
+        return;
+    };
+}*/
+
+for (let i in deck) {
+    deck[i].src = "img/Card.jpg";
+    deck[i].flipped = true;
+};
+
+function isPair() {
+    if (check1 === check2) {
+
+    }
+};
+
+function clickImg(e) {
+    if (e.target.flipped = true && flippedCard < 2) {
+        e.target.flipped = false;
+        e.target.src = "img/" + e.target.id + ".jpg";
+        flippedCard += 1;
+        console.log(e.target.id);
+    } else {
+        for (let i in deck) {
+            
+            deck[i].src = "img/Card.jpg";
+            deck[i].flipped = true;
+            flippedCard = 0;
+            
+        }
     }
 }
+
+
+console.log("Flipped status : " + deck[0].flipped);
 
