5df9b14bc7d1830ab39d3a55fd697fb4854026a0
Myosothis <myosothis1@gmail.com>
2024-08-25 14:29:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let flippedCard = 0; // Compteur de cartes retournées.
 let check1 = ''; // Vérification des pairs dans la fonction isPair.
 let check2 = '';
 let validCards = 0; // Vérifie le nombre de cartes "validées".
+let shownCard; // Annonce que la carte est affichée.
+let lockCard; // Annonce que la carte est verrouillée dans son état.
 
 // Récupération des images via leur class name depuis le HTML.
 let deck = document.getElementsByClassName("card");
@@ -35,16 +37,36 @@ function isPair() {
         check1 = '';
         check2 = '';
         validCards += 2;
+        for (let i in deck) {
+            if (deck[i].shownCard = true) {
+                shownCard = false;
+                lockCard = true;
+                deck.splice;
+            } else {
+            deck[i].src = "img/Card.jpg";
+            deck[i].flipped = true;
+            flippedCard = 0;
+            }
+        };
         winCondition();
     } else if (check1 !== '' && check2 !== '') {
         console.log("NOT PAIR");
         check1 = '';
         check2 = '';
+        for (let i in deck) {
+            deck[i].src = "img/Card.jpg";
+            deck[i].flipped = true;
+            }
     }
 };
 
 // Sélectionne les cartes et invoque isPair.
 function clickImg(e) {
+    for (let i in deck) {
+        if (shownCard = true) {
+           removeEventListener("click", clickImg);
+        }
+    }
     if (e.target.flipped = true && flippedCard <= 1) {
         e.target.flipped = false;
         e.target.src = "img/" + e.target.id + ".jpg";
@@ -52,37 +74,32 @@ function clickImg(e) {
         console.log(e.target.id);
             if (check1 === '') {
                 check1 = e.target.src;
+                e.target.shownCard = true;
             } else if (check1 !== '' && check2 === '') {
                 check2 = e.target.src;
+                e.target.shownCard = true;
                 isPair();
+                flippedCard = 0;  
             };
             
-    } else {
-        for (let i in deck) {
-
-            deck[i].src = "img/Card.jpg";
-            deck[i].flipped = true;
-            flippedCard = 0;
-            
-        }
     }
 };
 
 // Recommencer le jeu.
 function resetGame(){
-        for (let i in deck) {
+    for (let i in deck) {
 
-            deck[i].src = "img/Card.jpg";
-            deck[i].flipped = true;
-            flippedCard = 0;
-            
-        }
-    flippedCard = 0;
-    check1 = '';
-    check2 = '';
-    validCards = 0;
+        deck[i].src = "img/Card.jpg";
+        deck[i].flipped = true;
+        flippedCard = 0;
 
-    document.getElementById('victoire').classList.remove('show');
+    }
+flippedCard = 0;
+check1 = '';
+check2 = '';
+validCards = 0;
+
+document.getElementById('victoire').classList.remove('show');
 
 }
 document.querySelector('.start').addEventListener('click', resetGame);
